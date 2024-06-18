714ffec898cec68f01a36bd81ffba3f77c3680bc
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-06-19 10:36:55 +1200

    setup user.js

---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const user = require('../models/User')
+const auth = require('../models/auth')
+
+const router = express.Router()
+
+router.get('/me', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch {
+        res.status(500).send('Server Error')
+    }
+})
+
+module.exports = router
